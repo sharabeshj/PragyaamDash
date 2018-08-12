dcc370434d1433734a2a9d6016aac8f1b938e4af
sharabeshj <sharabeshjayaraman@gmail.com>
2018-08-12 16:57:14 +0530

    replce

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import BaseLayout from './containers/BaseLayout/BaseLayout';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BaseLayout />
+      hi
     );
   }
 }
