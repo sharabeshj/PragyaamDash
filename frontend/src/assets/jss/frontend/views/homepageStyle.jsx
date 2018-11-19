c2314c8ddee2f19f3b557e3256c302940be20edf
sharabeshj <sharabeshjayaraman@gmail.com>
2018-11-19 01:49:23 -0500

    dasboard customization --dev

---
+++ frontend/src/assets/jss/frontend/views/homepageStyle.jsx
@@ -0,0 +1,39 @@
+
+import { defaultFont } from "../../frontend";
+import modalStyle from "../modalStyle";
+
+const homepageStyle = theme => ({
+  cardTitle: {
+    marginTop: "0",
+    marginBottom: "3px",
+    color: "#3C4858",
+    fontSize: "18px"
+  },
+  cardHeader: {
+    zIndex: "3"
+  },
+  cardSubtitle: {
+    ...defaultFont,
+    color: "#999999",
+    fontSize: "14px",
+    margin: "0 0 10px"
+  },
+  center: {
+    textAlign: "center"
+  },
+  right: {
+    textAlign: "right"
+  },
+  left: {
+    textAlign: "left"
+  },
+  marginRight: {
+    marginRight: "5px"
+  },
+  modalSectionTitle: {
+    marginTop: "30px"
+  },
+  ...modalStyle(theme)
+});
+
+export default homepageStyle;
--- frontend/src/views/HomePage/HomePage.jsx
+++ frontend/src/views/HomePage/HomePage.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { withStyles } from '@material-ui/core/styles';
+import AddAlert from '@material-ui/icons/AddAlert';
+
+import GridContainer from '../../components/Grid/GridContainer';
+import GridItem from '../../components/Grid/GridItem';
+import CustomButton from '../../components/CustomButtons/Button';
+import Snackbar from '../../components/Snackbar/Snackbar';
+
+import homepageStyle from '../../assets/jss/frontend/views/homepageStyle';
+
 import {login} from '../../store/Actions/ActionCreator';
+import Axios from 'axios';
 
 class HomePage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            tc : false,
+            dashReportsdata : []
+        };
+    }
+
+    componentWillUnmount(){
+        let id = window.setTimeout(null, 0);
+        while(id --) {
+            window.clearInterval(id);
+        }
+    }
+
+    showNotification(place){
+        if(!this.state.tc){
+        
+            this.setState({ tc : true });
+            setTimeout(
+                function(){
+                    // console.log('hello');
+                    this.setState({ tc : false });
+                }.bind(this),
+                4000
+            );
+        }
+    }
+
     componentDidMount(){
 
         const loginData = {
@@ -13,12 +53,49 @@ class HomePage extends React.Component{
         };
         console.log(loginData);
         this.props.login(loginData);
+
+        const postData = {
+            method : 'GET',
+            url : 'http://127.0.0.1:8000/api/reports/',
+            auth : {
+                username : 'sharabesh',
+                password : 'shara1234'
+            }
+        };
+
+        Axios(postData)
+        .then(res => this.setState(() => {
+            let dashReportsdata = [];
+            dashReportsdata = res.data.map(report => {
+                if(report.reported && initial){
+                    return {
+                        ...report,
+                    }
+                }
+            })
+        }))
     }
-    render(){
-        if(this.props.loginState.token !== ''){
-            return (<div>login Success</div>)
+
+    componentDidUpdate(prevProps){
+        if(this.props.loginState.token !== '' && this.props.loginState.token !== prevProps.loginState.token){
+            this.showNotification('tc');
         }
-        return (<div>hi</div>);
+    }
+
+    render(){
+        
+
+        return (<div>
+            <Snackbar 
+                place="tc"
+                color="info"
+                icon = {AddAlert}
+                message="Login Sucess! Welcome to Pragyaam Dash"
+                open={this.state.tc}
+                closeNotification = { () => this.setState({ tc : false })}
+                close
+            />
+        </div>);
     }
 }
 
