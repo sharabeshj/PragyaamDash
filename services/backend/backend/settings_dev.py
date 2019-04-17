6e837dee70fd00c78ec971107a6b0d73da2c5ccc
sharabesh <sharabeshjayaraman@gmail.com>
2019-04-17 02:53:02 -0400

    changes requested v2

--- services/backend/backend/settings_dev.py
+++ services/backend/backend/settings_dev.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
